8e40ed9bf0ccff65ba41e2f261b298868960b404
Deb's <167595527+charmander2414@users.noreply.github.com>
2025-09-02 00:33:30 +0000

    feat : add projeto

---
+++ Televisao.cs
@@ -0,0 +1,13 @@
+public class Televisao
+{
+    public Televisao(float tamanho)
+    {
+        Tamanho = tamanho;
+    }
+
+    public float Tamanho { get; }
+    public int Resolucao { get; set; } 
+    public int Volume { get; set; }
+    public int Canal  { get; set; }
+    public bool Estado { get; set; }
+}
